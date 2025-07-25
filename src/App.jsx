6e0e9e36b9c85c0de6358cfe7ee2381fadbccf58
Vivek Mane <vivekmane172@gmail.com>
2025-07-25 20:49:27 +0530

    Completed Design using tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,30 @@ function App() {
   return (
     <>
     <Navbar/>
-    
+    <div className="container mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh]">
+      <div className="addTodo my-5">
+        <h2 className='text-lg font-bold'>Add a Todo</h2>
+        <input type="text" className='w-1/2 bg-white' />
+
+
+
+        <button className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-white rounded-md mx-6 text-sm font-bold'>Add</button>
+
+      </div>
+        <h2 className='text-lg font-bold bf'>Your Todos</h2>
+        <div className="todos">
+          <div className="todo flex">
+          <div className="text">Lorem ipsum dolor sit amet consectetur adipisicing.</div>
+            <div className="buttons">
+              <button className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-white rounded-md mx-1 text-sm font-bold'>Edit</button>
+              <button className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-white rounded-md mx-1 text-sm font-bold'>Delete</button>
+
+            
+          </div>
+          </div>
+        </div>
+        
+    </div>
 
     </>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,18 +2,13 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <nav className='flex justify-between bg-slate-700 text-white'>
-            {/* <div className="logo">
+        <nav className='flex justify-between bg-blue-700 text-white py-2'>
+            <div className="logo">
                 <span className='font-bold text-xl mx-7'>vTask</span>
-            </div> */}
-            <span className='font-bold text-xl mx-7'>vTask</span>
-            {/* <div className="logo">
-
-            </div> */}
-
+            </div>
             <ul className="flex gap-8 mx-9">
-                <li className='cursor-pointer'>Home</li>
-                <li className='cursor-pointer'>Your tasks</li>
+                <li className='cursor-pointer hover:font-bold transition-all'>Home</li>
+                <li className='cursor-pointer hover:font-bold transition-all'>Your tasks</li>
             </ul>
         </nav>
     )
